624a5ab03f15fa7a2597076bac23f5d80bbd04e1
joao.xavier <joaovitor55670@gmail.com>
2026-08-25 17:17:33 -0300

    adjust

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["design-system-mars"],
+  turbopack: {
+    root: path.join(__dirname, ".."),
+  },
 };
 
 export default nextConfig;
