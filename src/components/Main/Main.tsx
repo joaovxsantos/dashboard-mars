cbf86f58c12d4fef702828ac3e08f1879784adb7
joao.xavier <joaovitor55670@gmail.com>
2026-08-25 17:04:50 -0300

    feat: dashboard styles transition

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,14 @@
+'use client';
+import { usePathname } from 'next/navigation';
 import { MainProps } from '@/src/interface';
 import styles from './Main.module.scss';
 
 
 export function Main({ title, children }: MainProps) {
+    const pathname = usePathname();
 
     return (
-        <div className={styles["main-container"]}>
+        <div key={pathname} className={styles["main-container"]}>
             <h2 className={styles["main-title"]}>{title}</h2>
             {children}
         </div>
